52477d6ffe12b164c013400cfdfde1de3c95a0ec
Jason Sosa <hernamdezjason36@gmail.com>
2021-09-05 19:58:20 -0600

    l a L

--- DAWLab_Practica1/js/SegundaPracticas.js
+++ DAWLab_Practica1/js/SegundaPracticas.js
@@ -1,16 +1,16 @@
 var biblioteca = new Array();
 
 function GuradarLibro(obj) {
-    new Libro = new Object();
+    var Libro = new Object();
 
-    libro.ID = document.getElementById('txtID').value;
-    libro.titulo = document.getElementById('txttitulo').value;
-    libro.nombre = document.getElementById('txtnombre').value;
-    libro.apellido = document.getElementById('txtapellido').value;   
-    libro.genero = document.getElementById('txtgenero').value;
-    libro.pais = document.getElementById('txtpais').value;
-    libro.precio = document.getElementById('txtprecio').value;
-    libro.imagen = document.getElementById('img').value; 
+    Libro.ID = document.getElementById('txtID').value;
+    Llibro.titulo = document.getElementById('txttitulo').value;
+    Llibro.nombre = document.getElementById('txtnombre').value;
+    Llibro.apellido = document.getElementById('txtapellido').value;   
+    Llibro.genero = document.getElementById('txtgenero').value;
+    Llibro.pais = document.getElementById('txtpais').value;
+    Llibro.precio = document.getElementById('txtprecio').value;
+    Llibro.imagen = document.getElementById('img').value; 
     biblioteca.push(libro); 
     MostrarL();
 
