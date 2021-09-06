c1ce1e053ad7075779437be95c101caba165083b
Alexander0076 <uliseselias007@gmail.com>
2021-09-05 18:45:38 -0600

    numeros aleatorios del ejercicio 1

--- DAWLab_Practica1/js/PrimerEjercicio.js
+++ DAWLab_Practica1/js/PrimerEjercicio.js
@@ -1,37 +1,35 @@
-x = 0, y = 0;
-
-
 function Llenar_Matriz() {
    
     x = document.getElementById('filas').value;
     y = document.getElementById('columnas').value;
-    console.log(x,y);
-    var AlNu= new let[x,y]
+     max= 1;
+     min = 500;
+    table="";
 
-    
 
-    for(let filas = 0; filas<x; filas++ ){
-        for (let columnas = 0; columnas < y; columnas++) {
-            AlNu[filas,columnas]= Math.random()
-           
-        }
-    }
+            for (i = 0; i < x; i++)
+            {
+                for ( j = 0; j < y; j++)
+                {
+                    tabla[i, j] = Math.floor(Math.random() * (max - min + 1)) + min;;
+                }
+            }
+
+            for (i = 0; i < x; i++){
+            
+                table += "<tr>";
+                
+                
+                for ( j = 0; j < y; j++)
+                {
+                    table += "<td>"+tabla[i,j]+"</td>";
+                    
+                }
+                table += "</tr>";
+            }
+            document.getElementById('tabla').innerHTML=table;
+
+}
     
    
 
-
-}
-function Mostrar() {
-    tabla+="<table>";
-    for(let filas = 0; filas<x; filas++ ){
-        
-        for (let columnas = 0; columnas < y; columnas++) {
-            document.getElementById('tabla').innerHTML = AlNu[filas,columnas]
-           
-        }
-        
-        
-    }
-    
-
-}
