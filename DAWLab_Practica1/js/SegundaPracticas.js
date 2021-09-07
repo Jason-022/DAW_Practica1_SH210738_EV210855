64feb8713cb3b5e6c639b0ef8f5ffe2fe12f0e62
Jason Sosa <hernamdezjason36@gmail.com>
2021-09-06 19:10:13 -0600

    terminado

--- DAWLab_Practica1/js/SegundaPracticas.js
+++ DAWLab_Practica1/js/SegundaPracticas.js
@@ -5,42 +5,47 @@ function GuardarLibro() {
     var Libro = new Object();
     let IMG = new FileReader();
     IMG.readAsDataURL(document.getElementById('img').files[0]);
-    
+
     Libro.ID = document.getElementById('txtID').value;
     Libro.titulo = document.getElementById('txttitulo').value;
     Libro.nombre = document.getElementById('txtnombre').value;
-    Libro.apellido = document.getElementById('txtapellido').value;   
+    Libro.apellido = document.getElementById('txtapellido').value;
     Libro.genero = document.getElementById('txtgenero').value;
     Libro.pais = document.getElementById('txtpais').value;
     Libro.precio = document.getElementById('txtprecio').value;
-    Libro.imagen = IMG; 
-    
-    
-    biblioteca.push(Libro); 
-    
+    Libro.imagen = IMG;
     document.getElementById('formulario').reset();
-    MostrarL();
-     
+    MostrarL(Libro);
+
 
 
 }
 
-function MostrarL() {
-    
-    
+function MostrarL(Libro) {
+    let template = "";
 
-        for (var e in biblioteca) {
-            
-            biblioteca[e].imagen.onload = ()=>{
-                document.getElementById('imagen').src = biblioteca[e].imagen.result;
-             }
-             
-            
-            document.getElementById('Autor').innerHTML = biblioteca[e].nombre + biblioteca[e].apellido ;
-            document.getElementById('Libro').innerHTML = biblioteca[e].ID + biblioteca[e].titulo;
-            document.getElementById('Precio').innerHTML = biblioteca[e].precio;
-        }
-        
-    
-    console.log(biblioteca);
+
+
+    template += `
+            <div id="libro" class="col-md-6">
+            <div class="d-flex flex-row">
+            <img id="imagen${Libro.ID}" width="100px" height="100px"><br>
+            <div class="d-flex flex-column mx-2">
+            <label>Autor: ${Libro.nombre + " " + Libro.apellido}</label>
+            <label>Titulo: ${Libro.titulo}</label>          
+            <label> Precio: $ ${Libro.precio}</label>
+            </div>
+            </div>
+        </div>
+            `;
+    Libro.imagen.onload = () => {
+        document.getElementById('imagen'.concat(Libro.ID)).src = Libro.imagen.result;
+    }
+
+
+
+
+    document.getElementById('biblioteca').innerHTML += template;
+
+
 }
